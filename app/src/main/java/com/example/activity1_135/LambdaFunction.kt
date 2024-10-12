431980faae842d1ee72e26ce7738c674e98b9851
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-10-12 23:22:00 +0700

    Menambahkan contoh ekspresi lambda untuk mengubah string menjadi huruf besar

--- app/src/main/java/com/example/activity1_135/LambdaFunction.kt
+++ app/src/main/java/com/example/activity1_135/LambdaFunction.kt
@@ -9,3 +9,9 @@ fun uppercaseString(string: String): String {
     return string.uppercase()
 }
 
+//dapat ditulis dalam ekspresi lambda sbb:
+fun main() {
+    uppercaseString("Hello")
+    println({string: String -> string.uppercase() }("Hello"))
+// Hello
+}
