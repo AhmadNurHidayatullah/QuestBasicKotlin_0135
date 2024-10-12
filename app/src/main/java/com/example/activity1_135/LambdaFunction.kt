8ed455957717dee6119bbcf55d993343778f1f87
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-10-12 23:20:42 +0700

    Menambahkan fungsi uppercaseString untuk mengubah string menjadi huruf besar

--- app/src/main/java/com/example/activity1_135/LambdaFunction.kt
+++ app/src/main/java/com/example/activity1_135/LambdaFunction.kt
@@ -1,2 +1,11 @@
 package com.example.activity1_135
 
+// Lambda Functions
+// Lambda fungtions adalah fungsi yang tidak memiliki nama
+// lambda Fungtions biasanya digunakan untuk membuat fungsi yang sederhana
+// Lambda Fungtions menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
+
