60b61cbe6c0f1a3001bf7399e6d0bd3a10d1852b
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-10-12 23:44:36 +0700

    Menerapkan tipe data dasar di Kotlin: Byte, Int, Float, Double, Char, Boolean, dan String

---
+++ app/src/main/java/com/example/activity1_135/TipeData.kt
@@ -0,0 +1,27 @@
+package com.example.activity1_135
+
+fun main() {
+    // Byte
+    val ByteValue: Byte = 10
+    println("Byte: $ByteValue")
+
+    //int
+    val intValue: Int = 30
+    println("int: $intValue")
+
+    //float
+    val floatValue: Float = 50.6f
+    println("float: $floatValue")
+
+    // Double
+    val DoubleValue: Double = 60.6
+    println("Double: $DoubleValue")
+
+    //char
+    val charValue: Char = 'A'
+    println("char: $charValue")
+
+    // Boolean
+    val BooleanValue: Boolean = true
+    println("Boolean: $BooleanValue")
+
